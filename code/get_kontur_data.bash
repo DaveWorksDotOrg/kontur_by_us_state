c573e7bc48cf9f6f47ad08f3650c2789f59c9fc3
David Ferry <okidave2000@gmail.com>
2023-02-14 13:09:46 -0500

    Modified script to point to actual data file locations.

--- code/get_kontur_data.bash
+++ code/get_kontur_data.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget -P data/raw/ https://data.humdata.org/m/dataset/kontur-population-united-states-of-america
-wget -P data/raw/ https://data.humdata.org/m/dataset/kontur-boundaries-united-states
+wget -P data/raw/ https://geodata-eu-central-1-kontur-public.s3.amazonaws.com/kontur_datasets/kontur_population_US_20220630.gpkg.gz
+wget -P data/raw/ https://geodata-eu-central-1-kontur-public.s3.eu-central-1.amazonaws.com/kontur_datasets/kontur_boundaries_US_20220407.gpkg.gz
