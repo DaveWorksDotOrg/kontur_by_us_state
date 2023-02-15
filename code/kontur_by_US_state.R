21d806f3bca54342cb77fdf6575ccc51b94a0e4a
David Ferry <okidave2000@gmail.com>
2023-02-15 11:02:33 -0500

    Initial for loop to parse data. Closes #3.

--- code/kontur_by_US_state.R
+++ code/kontur_by_US_state.R
@@ -19,9 +19,6 @@ library(ggplot2)
 library(jsonlite)
 library(dplyr)
 
-<<<<<<< HEAD
-=======
-
 ################################################################################
 #                                  Load Data                                   #
 ################################################################################
@@ -54,3 +51,27 @@ census <- census %>%
 
 tigris_us <- st_transform(tigris_us, 3857)
 census <- st_transform(census, 3857)
+
+
+################################################################################
+#                     Initial parse of data by state                           #
+################################################################################
+
+# Create data frame of which kontur lines match with each state.
+# WARNING! This next code block took my system approximately 4 hours to run.
+
+state_list <- as.list(tigris_us$STUSPS[])
+ix <- data.frame()
+for (i in state_list) {
+  state <- tigris_us %>% filter(STUSPS == i)
+  ix_list <- list()
+  ix_list <- st_intersects(state, kontur)
+  ix_list <- cbind(i, ix_list)
+  ix <- rbind(ix, ix_list)
+}
+colnames(ix) <- c("state", "lines")
+
+# Steps to write to and read from a json file in case you need to let code run overnight
+address = "E:/Projects/ferry_konturbysate_2023/data/processed/kontur_US_tigris_intersections.json"
+ix_json <- toJSON(ix, dataframe = 'rows', pretty = TRUE)
+write(ix_json, address)
