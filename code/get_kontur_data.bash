3aa6e844de4b7b0b70421063abf851c9e141c5aa
David Ferry <okidave2000@gmail.com>
2023-02-15 16:12:39 -0500

    Updated script for downloading and unzipping kontur file.

--- code/get_kontur_data.bash
+++ code/get_kontur_data.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget -P data/raw/ https://geodata-eu-central-1-kontur-public.s3.amazonaws.com/kontur_datasets/kontur_population_US_20220630.gpkg.gz
-wget -P data/raw/ https://geodata-eu-central-1-kontur-public.s3.eu-central-1.amazonaws.com/kontur_datasets/kontur_boundaries_US_20220407.gpkg.gz
+wget -P data/raw/ -nc https://geodata-eu-central-1-kontur-public.s3.amazonaws.com/kontur_datasets/kontur_population_US_20220630.gpkg.gz
+unzip -n -d data/raw data/raw/kontur_population_US_20220630.gpkg.gz
